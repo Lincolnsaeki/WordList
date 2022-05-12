03492dc1411369773eab24b8de4f79b886439978
Rinnosuke Saeki <macrin1559@gmail.com>
2022-05-12 17:34:49 +0900

    push

---
+++ WordList/AddViewController.swift
@@ -0,0 +1,60 @@
+//
+//  AddViewController.swift
+//  WordList
+//
+//  Created by 佐伯凜乃介 on 2022/05/12.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet var englishTextField: UITextField!
+    @IBOutlet var japaneseTextField: UITextField!
+    
+    var wordArrey: [Dictionary<String, String>] = []
+    
+    let saveDate = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if saveDate.array(forKey: "WORD") != nil{
+            wordArrey = saveDate.array(forKey: "WORD") as! [Dictionary<String, String>]
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveWord(){
+        
+        let wordDictionary = ["english":englishTextField.text!, "japanese":japaneseTextField.text!]
+        
+        wordArrey.append(wordDictionary)
+        saveDate.set(wordArrey, forKey: "WORD")
+        
+        let alert = UIAlertController(
+        
+            title: "保存完了", message: "単語の登録が完了しました", preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        ))
+        present(alert, animated: true, completion: nil)
+        englishTextField.text = ""
+        japaneseTextField.text = ""
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WordList/QuestionViewController.swift
@@ -0,0 +1,67 @@
+//
+//  QuestionViewController.swift
+//  WordList
+//
+//  Created by 佐伯凜乃介 on 2022/05/12.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    @IBOutlet var answerLabel:UILabel!
+    @IBOutlet var questionLabel:UILabel!
+    @IBOutlet var nextButton: UIButton!
+    
+    
+    @IBAction func nextBottonTapped (){
+        
+        if isAnswered {
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            if nowNumber < wordArrey.count{
+                questionLabel.text = wordArrey[nowNumber]["english"]
+                isAnswered = false
+                nextButton.setTitle("答えを表示", for: .normal)
+            }else{
+                nowNumber = 0
+                performSegue(withIdentifier: "toFinishView", sender: nil)
+            }
+        }else{
+            answerLabel.text = wordArrey[nowNumber]["japanese"]
+            isAnswered = true
+            nextButton.setTitle("次へ", for: .normal)
+        }
+        
+    }
+    
+    var isAnswered:Bool = false
+    var wordArrey: [Dictionary<String, String>] = []
+    var nowNumber: Int = 0
+    let saveDate = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        wordArrey = saveDate.array(forKey: "WORD") as! [Dictionary<String, String>]
+        
+        wordArrey.shuffle()
+        questionLabel.text = wordArrey[nowNumber]["english"]
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
